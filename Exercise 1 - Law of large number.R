0f679eae5e812e473f0952ef37301d14e81f127f
vadukia2 <77515069+vadukia2@users.noreply.github.com>
2021-02-03 22:42:48 -0600

    Update Exercise 1 - Law of large number.R
    # The law of large numbers, in probability and statistics, states that as a sample size grows, its mean gets closer to the average of the whole population.
    #In a financial context, the law of large numbers indicates that a large entity that is growing rapidly cannot maintain that growth pace forever
    # The above code just illustrates an example of the law of large numbers when applied to normally distributed data of random numbers. The higher the number of #observations lying between 1 and -1.. As per the law of large numbers, 68% of the distribution should lie between 1 and -1.

--- Exercise 1 - Law of large number.R
+++ Exercise 1 - Law of large number.R
@@ -11,3 +11,8 @@ for (i in rnorm(N)) {
 
 answer <- counter/ N 
 answer
+
+# The law of large numbers, in probability and statistics, states that as a sample size grows, its mean gets closer to the average of the whole population.
+#In a financial context, the law of large numbers indicates that a large entity which is growing rapidly cannot maintain that growth pace forever
+# The above code just illustrates a example of law of large numbers when applied to  a normally distributed data of random number, the higher the number of observation lying between 1 and -1.
+#As per law of large number 68% of the distribution should lie between 1 and -1.
